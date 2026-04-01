24058a18a44db3f6043b5ee446350ed53038af66
pielc <pierre.le.chanu.pro@gmail.com>
2026-04-01 19:32:49 +0200

    new start

--- init.lua
+++ init.lua
@@ -1,2 +1,19 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+
+vim.g.mapleader = " "
+
 require("config.lazy")
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader><leader>', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+
+require('nvim-treesitter').setup ({
+  ensure_installed = {"lua", "javascript"},
+  highlight = { enable = true },
+  indent = { enable = true },
+})
--- lua/plugins/dogrun.lua
+++
@@ -1,9 +0,0 @@
-return {
-  {
-    "wadackel/vim-dogrun",
-    priority = 1000, -- load before other UI plugins
-    config = function()
-      vim.cmd("colorscheme dogrun")
-    end,
-  },
-}
