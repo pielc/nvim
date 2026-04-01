9a480d4a1807a5f366368c752a861f93a20d54af
pielc <pierre.le.chanu.pro@gmail.com>
2026-04-01 22:11:54 +0200

    add neotree and lualine

--- init.lua
+++ init.lua
@@ -2,6 +2,10 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
+vim.cmd("set number")
+-- vim.cmd("set number")
+vim.wo.relativenumber = true
+vim.cmd("set cmdheight=0")
 
 vim.g.mapleader = " "
 
@@ -12,8 +16,20 @@ vim.keymap.set('n', '<leader><leader>', builtin.find_files, { desc = 'Telescope 
 vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
 
+vim.keymap.set('n', '<leader>e', ':Neotree position=float<CR>')
+
 require('nvim-treesitter').setup ({
   ensure_installed = {"lua", "javascript"},
   highlight = { enable = true },
   indent = { enable = true },
 })
+
+local noirbuddy_lualine = require("noirbuddy.plugins.lualine")
+
+require("lualine").setup {
+  options = {
+    theme = noirbuddy_lualine.theme,
+  },
+  sections = noirbuddy_lualine.sections,
+  inactive_sections = noirbuddy_lualine.inactive_sections,
+}
--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -52,6 +52,20 @@ require("lazy").setup({
      'nvim-treesitter/nvim-treesitter',
       lazy = false,
       build = ':TSUpdate'
+    },
+    {
+      "nvim-neo-tree/neo-tree.nvim",
+      branch = "v3.x",
+      dependencies = {
+        "nvim-lua/plenary.nvim",
+        "MunifTanjim/nui.nvim",
+        "nvim-tree/nvim-web-devicons", -- optional, but recommended
+      },
+      lazy = false, -- neo-tree will lazily load itself
+    },
+    {
+      'nvim-lualine/lualine.nvim',
+      dependencies = { 'nvim-tree/nvim-web-devicons' }
     }
   },
   -- Configure any other settings here. See the documentation for more details.
